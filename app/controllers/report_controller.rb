24ad748a90f94f1559e5df5c16fbc7c11397f8ab
Aaron McLeod <aaronmcleod@Aaron-McLeods-MacBook-Pro.local>
2010-06-15 17:28:11 -0400

    added exception checking

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -6,7 +6,12 @@ class ReportController < ApplicationController
     if request.post?
       @report = Report.new(params[:report])
       if @report.valid?
-        @report.content = Net::HTTP.get(URI.parse(params[:report][:domain]))
+        begin
+          @report.content = Net::HTTP.get(URI.parse(params[:report][:domain]))
+        rescue Exception:
+          flash[:error] = "An error occured parsing the given URL. Please check that the URL you provided is correct."
+          @report.content = ""
+        end
       else
         render :action => 'index'
       end
